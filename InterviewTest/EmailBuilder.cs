813796400d2ee4553104c77832ffb6788541a31f
pholubovych <pavlo.holubovych@codeworldwide.com>
2020-04-22 14:13:29 +0300

    UT

---
+++ InterviewTest/EmailBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace InterviewTest
+{
+    public class WelcomeEmailBuilder
+    {
+        public string Build(string name, string permissions)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(GetHeader(name));
+            builder.Append(GetBody(permissions));
+            builder.Append(GetFooter());
+            return builder.ToString();
+        }
+
+        private string GetHeader(string name)
+        {
+            return $"Welcome to our system {name}";
+        }
+
+        private string GetBody(string permissions)
+        {
+            return $"Your account was created at {DateTime.Now.Date} by our Admin and granted you {permissions} permissions";
+        }
+
+        private string GetFooter()
+        {
+            return $"Best regards, perfect system team";
+        }
+    }
+}
---
+++ InterviewTest/EmailBuilderTest.cs
@@ -0,0 +1,10 @@
+using System;
+using Xunit;
+
+namespace InterviewTest
+{
+    public class EmailBuilderTest
+    {
+       
+    }
+}
